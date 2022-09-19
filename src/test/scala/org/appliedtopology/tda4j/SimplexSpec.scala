243aed1e81c47d0534230a0e396467a689fa8325
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2022-09-19 19:56:25 -0400

    Test suite setup

--- src/test/scala/org/appliedtopology/tda4j/SimplexSpec.scala
+++ src/test/scala/org/appliedtopology/tda4j/SimplexSpec.scala
@@ -4,7 +4,7 @@ import org.appliedtopology.tda4j.SimplexSpec.{beGreaterThan, contain, s2}
 import org.specs2.Specification
 import org.specs2.execute.Result
 
-object SimplexSpec extends Specification :
+object SimplexSpec extends Specification:
   def is =
     s2"""
 Checking the `Simplex` class properties.
@@ -14,8 +14,9 @@ A `Simplex` should
   have a boundary $e2
 """
 
-  def s : Simplex = Simplex(1, 2, 3)
+  def s: Simplex = Simplex(1, 2, 3)
 
   def e1: Result = s.size must beGreaterThan(0)
 
-  def e2: Result = s.boundary() must contain(Simplex(1, 2), Simplex(1, 3), Simplex(2, 3))
+  def e2: Result =
+    s.boundary() must contain(Simplex(1, 2), Simplex(1, 3), Simplex(2, 3))
