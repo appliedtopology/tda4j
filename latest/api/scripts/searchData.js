129a69b96e2f345ea7aca00b085a78d1e23e7c64
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2022-09-19 22:42:34 -0400

    updated site

---
+++ latest/api/scripts/searchData.js
@@ -0,0 +1,4 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"TDA4j","t":"TDA4j","d":"","k":"static"},
+{"l":"org/appliedtopology/tda4j.html#","e":false,"i":"","n":"org.appliedtopology.tda4j","t":"org.appliedtopology.tda4j","d":"","k":"package"},
+{"l":"org/appliedtopology/tda4j/Simplex.html#","e":false,"i":"","n":"Simplex","t":"Simplex(vertices: Int*) extends SortedSet[Int]","d":"org.appliedtopology.tda4j","k":"class"},
+{"l":"org/appliedtopology/tda4j/Simplex.html#boundary-fffffc52","e":false,"i":"","n":"boundary","t":"boundary(): List[Simplex]","d":"org.appliedtopology.tda4j.Simplex","k":"def"}];
