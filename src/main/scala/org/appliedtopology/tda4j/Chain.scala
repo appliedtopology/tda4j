58a33dd802a2bd7f053e98a4f440b8db8a1bf106
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2022-11-14 13:48:10 -0500

    Moved `Chain` to its own file

---
+++ src/main/scala/org/appliedtopology/tda4j/Chain.scala
@@ -0,0 +1,41 @@
+package org.appliedtopology.tda4j
+
+
+/**
+ * The Chain class is a representation of a formal linear combination of the n cells in a cell complex.
+ * Note that CellT is a type parameter subtype of Cell, a trait in this library. We have Scala look for
+ * a type parameter of cellOrdering that matches as best as possible an Ordering on CellT
+ *
+ * (Note: write a fully fleshed out explanation in comments after code def. write up)
+ *
+ *
+ * @tparam CellT Type of the cells in the chain complex. For example `AbstractSimplex[Int]` etc.
+ * @tparam CoefficientT Type of the coefficients of the chain complex. For example `Double` or `IntModp`
+ * @param Cell
+ *
+ *
+ *
+ */
+class Chain[CellT <: Cell, CoefficientT: Fractional[T]](using cellOrdering : Ordering[CellT])
+
+
+{
+
+}
+
+/** Lightweight trait to define what it means to be a topological "Cell".
+ */
+trait Cell {
+
+  /** The boundary map of the cell.
+   *
+   * @return
+   *   A sequence of boundary cells.
+   * @todo
+   *   When a `Chain` trait or class has been created, this should change to
+   *   return an appropriate `Chain` instead of the current `List`.
+   */
+
+
+  def boundary[CoefficientT : Fractional](): Chain[Cell, CoefficientT]
+}
--- src/main/scala/org/appliedtopology/tda4j/Simplex.scala
+++ src/main/scala/org/appliedtopology/tda4j/Simplex.scala
@@ -11,45 +11,6 @@ import scala.math.Ordering.Double.IeeeOrdering
 import scala.math.Ordering.IntOrdering
 
 
-/**
- * The Chain class is a representation of a formal linear combination of the n cells in a cell complex.
- * Note that CellT is a type parameter subtype of Cell, a trait in this library. We have Scala look for
- * a type parameter of cellOrdering that matches as best as possible an Ordering on CellT
- *
- * (Note: write a fully fleshed out explaination in comments after code def. write up)
- *
- *
- * @tparam CellT
- * @tparam CoefficientT
- * @param Cell
- *
- *
- *
- */
-class Chain[CellT <: Cell, CoefficientT: Fractional[T]](using cellOrdering : Ordering[CellT])
-
-
-{
-
-}
-
-/** Lightweight trait to define what it means to be a topological "Cell".
-  */
-trait Cell {
-
-  /** The boundary map of the cell.
-    *
-    * @return
-    *   A sequence of boundary cells.
-    * @todo
-    *   When a `Chain` trait or class has been created, this should change to
-    *   return an appropriate `Chain` instead of the current `List`.
-    */
-
-
-  def boundary(): Seq[Cell]
-}
-
 
 /** Type alias creating `Simplex` as the type representing
   * `AbstractSimplex[Int]`
