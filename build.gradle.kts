c36175789d29a766644faf38cc4adde6fdbac9de
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2023-11-08 23:31:39 -0800

    build.gradle.kts rebuilt from ground up to ensure MVJ understands how everything connects.
    Also, shadowJar plugin and task introduced to produce single JAR file with both library and all dependencies. We now get a single JAR file that can be loaded as library and contains everything the code needs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
-import org.jetbrains.dokka.gradle.DokkaTask
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("multiplatform") version "1.9.20"
+    `java`
+    `java-library`
+    `java-library-distribution`
     id("org.jetbrains.dokka") version "1.9.10"
     id("io.kotest.multiplatform") version "5.7.2"
-    id("com.google.devtools.ksp") version "1.8.21-1.0.11"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
-    `java-library-distribution`
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.appliedtopology"
-version = "1.0-SNAPSHOT"
+version = "0.1-DEV"
 
 distributions {
     main {
@@ -23,7 +25,64 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/mipt-npm/p/sci/maven")
 }
 
-tasks.withType<DokkaTask>().configureEach {
+kotlin {
+    jvmToolchain(17)
+
+    jvm {
+        // We have to configure the shadowJar task explicitly, because
+        // we are doing a multi-platform build
+        // https://stackoverflow.com/questions/63426211/kotlin-multiplatform-shadowjar-gradle-plugin-creates-empty-jar
+        compilations.named("main") {
+            tasks {
+                val shadowJar =
+                    register<ShadowJar>("ShadowJar") {
+                        group = "distribution"
+                        from(output)
+                        configurations = listOf(runtimeDependencyFiles)
+                        archiveClassifier.set("all")
+                        dependsOn("jvmJar")
+                        dependsOn("jvmMainClasses")
+                    }
+            }
+        }
+    }
+
+    js(IR) {
+        browser()
+        nodejs()
+    }
+
+    val hostOS = System.getProperty("os.name")
+    when {
+        hostOS.startsWith("Windows") -> mingwX64("native")
+        hostOS == "Linux" -> linuxX64("native")
+        else -> println("Not configured to build targeting native platform $hostOS")
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("io.arrow-kt:arrow-core:1.2.0")
+                implementation("space.kscience:kmath-core:0.3.1")
+                implementation("space.kscience:kmath-tensors:0.3.1")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation("io.kotest:kotest-framework-engine:5.7.2")
+                implementation("io.kotest:kotest-assertions-core:5.7.2")
+                implementation("io.kotest:kotest-property:5.7.2")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation("io.kotest:kotest-runner-junit5:5.7.2")
+            }
+        }
+    }
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
     dependencies {
         dokkaPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.10")
         dokkaPlugin("org.jetbrains.dokka:mathjax-plugin:1.9.10")
@@ -43,7 +102,8 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
     outputColorName.set("RED")
     ignoreFailures.set(true)
     enableExperimentalRules.set(true)
-    additionalEditorconfig.set( // not supported until ktlint 0.49
+    additionalEditorconfig.set(
+        // not supported until ktlint 0.49
         mapOf(
             "ktlint_standard_no-wildcard-imports" to "disabled",
         ),
@@ -51,68 +111,5 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
     filter {
         exclude("**/generated/**")
         include("**/kotlin/**")
-    }
-}
-
-kotlin {
-    jvmToolchain(17)
-    jvm {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-        }
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
-    js(IR) {
-        browser {
-            commonWebpackConfig {
-                // cssSupport.enabled = true
-            }
-        }
-    }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget =
-        when {
-            // hostOs == "Mac OS X" -> macosX64("native")
-            hostOs == "Linux" -> linuxX64("native")
-            isMingwX64 -> mingwX64("native")
-            else -> println("Host OS is not supported in Kotlin/Native.")
-        }
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(kotlin("stdlib"))
-                implementation(kotlin("script-runtime"))
-                implementation(platform("io.arrow-kt:arrow-stack:1.2.0"))
-                implementation("io.arrow-kt:arrow-core")
-                implementation("io.arrow-kt:arrow-fx-coroutines")
-                implementation("io.arrow-kt:arrow-optics")
-                api("space.kscience:kmath-core:0.3.1")
-                api("space.kscience:kmath-tensors:0.3.1")
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation("io.kotest:kotest-framework-engine:5.7.2")
-                implementation("io.kotest:kotest-assertions-core:5.7.2")
-                implementation("io.kotest:kotest-property:5.7.2")
-            }
-        }
-        val jvmMain by getting {
-            dependencies {
-                implementation(kotlin("script-runtime"))
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation("io.kotest:kotest-runner-junit5:5.7.2")
-            }
-        }
     }
 }
