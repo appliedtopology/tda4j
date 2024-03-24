5b678261e51a8cc87fb8c69466239cd886a3f819
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2024-03-24 10:45:55 +0100

    Starting to sketch out Ripser's approaches.

--- src/main/scala/org/appliedtopology/tda4j/RipserStream.scala
+++ src/main/scala/org/appliedtopology/tda4j/RipserStream.scala
@@ -116,6 +116,105 @@ class RipserCliqueFinder extends CliqueFinder[Int] {
     RipserStream(metricSpace, maxFiltrationValue, maxDimension).iterator.toSeq
 }
 
+
+class RipserStreamSparse(
+  val metricSpace: FiniteMetricSpace[Int],
+  val maxFiltrationValue: Double,
+  val maxDimension: Int = 2
+                        ) extends SimplexStream[Int,Double] {
+  given sc: SimplexContext[Int]()
+  import sc.*
+
+  //given Ordering[Simplex] = Ordering.by(filtrationValue).orElse(sc.given_Ordering_Simplex)
+  val doubleSimplexPairOrdering: Ordering[(Double,Simplex)] = { (x:(Double,Simplex),y:(Double,Simplex)) =>
+    Ordering.Double.TotalOrdering.compare(x._1,y._1) match {
+      case 0 => sc.given_Ordering_Simplex.compare(x._2, y._2)
+      case c:Int => c
+    }
+  }
+
+  given Ordering[(Double,Simplex)] = doubleSimplexPairOrdering
+
+  val si = SimplexIndexing(metricSpace.size)
+
+  override def filtrationValue: PartialFunction[AbstractSimplex[Int], Double] =
+    FiniteMetricSpace.MaximumDistanceFiltrationValue[Int](metricSpace)
+
+  def zeroPivotCofacet(index: Int, size: Int): Option[Int] = (for {
+    cofacet <- si.cofacetIterator(index, size, false)
+    if filtrationValue(si(cofacet, size + 1)) == filtrationValue(
+      si(index, size)
+    )
+  } yield cofacet).maxOption
+
+  def zeroPivotFacet(index: Int, size: Int): Option[Int] = (for {
+    facet <- si.facetIterator(index, size)
+    if filtrationValue(si(facet, size - 1)) == filtrationValue(si(index, size))
+  } yield facet).maxOption
+
+  def zeroApparentCofacet(index: Int, size: Int): Option[Int] =
+    for {
+      cofacet <- zeroPivotCofacet(index, size)
+      facet <- zeroPivotFacet(cofacet, size + 1)
+      if facet == index
+    } yield cofacet
+
+  def zeroApparentFacet(index: Int, size: Int): Option[Int] =
+    for {
+      facet <- zeroPivotFacet(index, size)
+      cofacet <- zeroPivotCofacet(facet, size - 1)
+      if facet == index
+    } yield cofacet
+
+  lazy val kruskal = Kruskal(metricSpace)
+
+  def zeroPersistence[CoefficientT]()
+  : List[PersistenceBar[Double, Chain[Simplex, CoefficientT]]] =
+    kruskal.mstIterator.map { (b, d) =>
+      PersistenceBar[Double, Chain[Simplex, CoefficientT]](
+        0,
+        ClosedEndpoint(0.0),
+        OpenEndpoint(metricSpace.distance(b, d))
+      )
+    }.toList
+
+  var simplexCacheContains: Option[Int] = None
+  val simplexCache: mutable.SortedSet[(Double,Simplex)] = mutable.SortedSet()
+
+  def iteratorByDimension(d: Int): Iterator[Simplex] = d match {
+    case 0 => metricSpace.elements.iterator.map { Simplex(_) }
+    case 1 => kruskal.cyclesIterator.map { (i,j) => Simplex(i,j) }
+    case dim: Int => {       // this is where the real work happens
+      // This can get expensive if we are for any reason NOT traversing dimension by dimension
+      if(simplexCacheContains != Some(d-1)) {
+        simplexCache.addAll(iteratorByDimension(d-1).map(s => (filtrationValue(s),s)))
+      }
+      for
+        fV <- simplexCache.map(_._1).iterator
+        previousSimplex <- simplexCache.filter(_._1 < fV).iterator.map(_._2)
+        nextVertex <- metricSpace.elements
+          .filter(!previousSimplex.contains(_))
+          .filter(nV => previousSimplex.map(oV => metricSpace.distance(oV,nV)).max <= fV)
+        simplex:Simplex <- List(previousSimplex+nextVertex)
+        if(zeroApparentCofacet(si(simplex),simplex.size).isEmpty)
+        if(zeroApparentFacet(si(simplex),simplex.size).isEmpty)
+        // also check if this is cleared?
+      yield
+        simplex
+    }.iterator
+  }
+
+  override def iterator: Iterator[Simplex] =
+    for
+      d <- (0 until maxDimension).iterator
+      s <- iteratorByDimension(d)
+    yield
+      s
+}
+
+
+
+
 abstract class RipserStreamBase(
   val metricSpace: FiniteMetricSpace[Int],
   val maxFiltrationValue: Double = Double.PositiveInfinity,
