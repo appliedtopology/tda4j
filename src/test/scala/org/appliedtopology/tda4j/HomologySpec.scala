76a3cadebe8be1c47d0ce1450a3a9d9ff0840dfc
huhwhy <daniel.bosah@stu.bcc.cuny.edu>
2024-04-06 14:31:35 -0400

    got reduceByBasis working, but boundary check is not completed YET. Trying to figure out how to get Chain to work outside the TDAContext and not have to use an extension method to make it work

--- src/test/scala/org/appliedtopology/tda4j/HomologySpec.scala
+++ src/test/scala/org/appliedtopology/tda4j/HomologySpec.scala
@@ -7,6 +7,7 @@ import org.specs2.mutable.Specification
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
 import scala.math
+import scala.math.Numeric.DoubleIsFractional
 import scala.runtime.Tuple2Zipped
 
 
@@ -22,15 +23,18 @@ class HomologySpec extends Specification {
       val subPointSet4 = Seq[Double](a, b)
       val points = Seq(subPointSet1, subPointSet2, subPointSet3, subPointSet4) //why a Sequence? Is it because they have an order?
 
-      //Start of setup for PH Barcode Check
-      //VR Setup
-      val metric = EuclideanMetricSpace(points)
-      val computationsOfEuclideanSpaceSansSqrt = a * a + b * b //distance between 2 points in ES in 2D sans sqrt. Needed so I can use in pow
-      val testValuesFormaxFiltrationValueEuclideanSpace = List[Double](scala.math.pow(computationsOfEuclideanSpaceSansSqrt, 0.5)).max
-      val simplices = VietorisRips[Int](metric, testValuesFormaxFiltrationValueEuclideanSpace, 3)
 
-      //what do I want to do here? Use given so I can use Chain class to specify coeff and VertexT
       given chain_ctx: TDAContext[Int, Double] with {
+
+
+        //Start of setup for PH Barcode Check
+        //VR Setup
+        val metric = EuclideanMetricSpace(points)
+        val computationsOfEuclideanSpaceSansSqrt = a * a + b * b //distance between 2 points in ES in 2D sans sqrt. Needed so I can use in pow
+        val testValuesFormaxFiltrationValueEuclideanSpace = List[Double](scala.math.pow(computationsOfEuclideanSpaceSansSqrt, 0.5)).max
+        val simplices = VietorisRips[Int](metric, testValuesFormaxFiltrationValueEuclideanSpace, 3)
+
+        //what do I want to do here? Use given so I can use Chain class to specify coeff and VertexT
 
         import chain_ctx.{given, *} //used to import all functionality of variable (chain.ctx) that uses TDAContext
 
@@ -38,28 +42,26 @@ class HomologySpec extends Specification {
         val cycles: ListBuffer[Chain[Simplex, Double]] = ListBuffer[Chain[Simplex, Double]]()
         val intervals: ListBuffer[(Simplex, Double)] = ListBuffer[(Simplex, Double)]() //returns tuple of interval
 
+
         extension (chain: Chain[Simplex, Double]) {
-          def reduceByBasis(basis: ListBuffer[Chain[Simplex, Double]]): Chain[Simplex, Double] = {
+          def reduceByBasis(basis: List[MapChain[Simplex, Double]]): Chain[Simplex, Double] = {
             var changed: Boolean = true
-            var sigma = this
+            var sigma: MapChain[Simplex, Double] = sigma //working on current chain
             while (changed) {
               changed = false
               for (basisElement <- basis) {
-                if (sigma.leadingTerm == basisElement.leadingTerm) {
-                  sigma = (basisElement.leadingCoefficient / sigma.leadingCoefficient) * sigma -
-                    (sigma.leadingCoefficient / basisElement.leadingCoefficient) * basisElement
+                if (sigma.leadingCell == basisElement.leadingCell) {
+                  val factor1: Double = basisElement.leadingCoefficient / sigma.leadingCoefficient //doing ops for boundary finding, just broken up to be easier to do
+                  val factor2: Double = sigma.leadingCoefficient / basisElement.leadingCoefficient
+                  sigma = scale(factor1, sigma) - scale(factor2, basisElement) //using Chain's scale to do multiplication of coeff and chain, since scala can't work with these datatypes natively
                   changed = true
+
                 }
               }
             }
             sigma
-
-
           }
-
         }
-
-
 
         //TDAContext is great! Due to the context-driven paradigm, the tda package object extends ChainOps and SimplexOps,
         //thus you don't have to keep on rewriting parameter names and function calls. Instead its assigned to a singular variable, that does all that in the backend
@@ -69,18 +71,6 @@ class HomologySpec extends Specification {
       }
 
 
-
-
-
-
-
-
     }
 
   }
-
-}
-
-
-
-
