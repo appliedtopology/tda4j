627b3c61ae03a427551ead9f9434fe64e5e24dc2
Mikael Vejdemo-Johansson <mikael@johanssons.org>
2024-04-07 04:21:38 -0400

    Fixing the TDAContext invocation
    Try this instead. Edits done on the web, has not been tried in practice.

--- src/test/scala/org/appliedtopology/tda4j/HomologySpec.scala
+++ src/test/scala/org/appliedtopology/tda4j/HomologySpec.scala
@@ -24,53 +24,53 @@ class HomologySpec extends Specification {
       val points = Seq(subPointSet1, subPointSet2, subPointSet3, subPointSet4) //why a Sequence? Is it because they have an order?
 
 
-      given chain_ctx: TDAContext[Int, Double] with {
+      given chain_ctx: TDAContext[Int, Double]()
+      import chain_ctx.{given, *} //used to import all functionality of variable (chain.ctx) that uses TDAContext
 
 
-        //Start of setup for PH Barcode Check
-        //VR Setup
-        val metric = EuclideanMetricSpace(points)
-        val computationsOfEuclideanSpaceSansSqrt = a * a + b * b //distance between 2 points in ES in 2D sans sqrt. Needed so I can use in pow
-        val testValuesFormaxFiltrationValueEuclideanSpace = List[Double](scala.math.pow(computationsOfEuclideanSpaceSansSqrt, 0.5)).max
-        val simplices = VietorisRips[Int](metric, testValuesFormaxFiltrationValueEuclideanSpace, 3)
+      //Start of setup for PH Barcode Check
+      //VR Setup
+      val metric = EuclideanMetricSpace(points)
+      val computationsOfEuclideanSpaceSansSqrt = a * a + b * b //distance between 2 points in ES in 2D sans sqrt. Needed so I can use in pow
+      val testValuesFormaxFiltrationValueEuclideanSpace = List[Double](scala.math.pow(computationsOfEuclideanSpaceSansSqrt, 0.5)).max
+      val simplices = VietorisRips[Int](metric, testValuesFormaxFiltrationValueEuclideanSpace, 3)
 
-        //what do I want to do here? Use given so I can use Chain class to specify coeff and VertexT
-
-        import chain_ctx.{given, *} //used to import all functionality of variable (chain.ctx) that uses TDAContext
-
-        val boundaries: ListBuffer[Chain[Simplex, Double]] = ListBuffer[Chain[Simplex, Double]]() //empty list buffer created
-        val cycles: ListBuffer[Chain[Simplex, Double]] = ListBuffer[Chain[Simplex, Double]]()
-        val intervals: ListBuffer[(Simplex, Double)] = ListBuffer[(Simplex, Double)]() //returns tuple of interval
+      //what do I want to do here? Use given so I can use Chain class to specify coeff and VertexT
 
 
-        extension (chain: Chain[Simplex, Double]) {
-          def reduceByBasis(basis: List[MapChain[Simplex, Double]]): Chain[Simplex, Double] = {
-            var changed: Boolean = true
-            var sigma: MapChain[Simplex, Double] = sigma //working on current chain
-            while (changed) {
-              changed = false
-              for (basisElement <- basis) {
-                if (sigma.leadingCell == basisElement.leadingCell) {
-                  val factor1: Double = basisElement.leadingCoefficient / sigma.leadingCoefficient //doing ops for boundary finding, just broken up to be easier to do
-                  val factor2: Double = sigma.leadingCoefficient / basisElement.leadingCoefficient
-                  sigma = scale(factor1, sigma) - scale(factor2, basisElement) //using Chain's scale to do multiplication of coeff and chain, since scala can't work with these datatypes natively
-                  changed = true
+      val boundaries: ListBuffer[Chain[Simplex, Double]] = ListBuffer[Chain[Simplex, Double]]() //empty list buffer created
+      val cycles: ListBuffer[Chain[Simplex, Double]] = ListBuffer[Chain[Simplex, Double]]()
+      val intervals: ListBuffer[(Simplex, Double)] = ListBuffer[(Simplex, Double)]() //returns tuple of interval
 
-                }
+
+      extension (chain: Chain[Simplex, Double]) {
+        def reduceByBasis(basis: List[MapChain[Simplex, Double]]): Chain[Simplex, Double] = {
+          var changed: Boolean = true
+          var sigma: MapChain[Simplex, Double] = sigma //working on current chain
+          while (changed) {
+            changed = false
+            for (basisElement <- basis) {
+              if (sigma.leadingCell == basisElement.leadingCell) {
+                val factor1: Double = basisElement.leadingCoefficient / sigma.leadingCoefficient //doing ops for boundary finding, just broken up to be easier to do
+                val factor2: Double = sigma.leadingCoefficient / basisElement.leadingCoefficient
+                sigma = scale(factor1, sigma) - scale(factor2, basisElement) //using Chain's scale to do multiplication of coeff and chain, since scala can't work with these datatypes natively
+                changed = true
+
               }
             }
-            sigma
           }
+          sigma
         }
-
-        //TDAContext is great! Due to the context-driven paradigm, the tda package object extends ChainOps and SimplexOps,
-        //thus you don't have to keep on rewriting parameter names and function calls. Instead its assigned to a singular variable, that does all that in the backend
-        //of TDAContext, which itself goes into the backend of ChainOps and SimplexContext. SC sets up the functionality to work with simplexes in a practical matter
-
-
       }
+
+      //TDAContext is great! Due to the context-driven paradigm, the tda package object extends ChainOps and SimplexOps,
+      //thus you don't have to keep on rewriting parameter names and function calls. Instead its assigned to a singular variable, that does all that in the backend
+      //of TDAContext, which itself goes into the backend of ChainOps and SimplexContext. SC sets up the functionality to work with simplexes in a practical matter
 
 
     }
 
+
   }
+
+}
