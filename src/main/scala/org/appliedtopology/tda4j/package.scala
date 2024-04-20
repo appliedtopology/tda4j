4a31d07d7e13bcbb9fb3f1c8411c7398740bcbc2
huhwhy <daniel.bosah@stu.bcc.cuny.edu>
2024-04-19 23:18:34 -0400

    resolving tab issue

--- src/main/scala/org/appliedtopology/tda4j/package.scala
+++ src/main/scala/org/appliedtopology/tda4j/package.scala
@@ -12,18 +12,12 @@ import org.appliedtopology.tda4j.barcode.{
 import math.Ordering.Implicits.sortedSetOrdering
 
 /** Package for the Scala library TDA4j
-  */
+ */
 package object tda4j {
   class TDAContext[VertexT: Ordering, CoefficientT: Fractional]
-      extends ChainOps[AbstractSimplex[VertexT], CoefficientT],
-        SimplexContext[VertexT] {
-<<<<<<< HEAD
-
-    given Conversion[Simplex, MapChain[Simplex, CoefficientT]] = MapChain.apply
-    
-=======
+    extends ChainOps[AbstractSimplex[VertexT], CoefficientT],
+      SimplexContext[VertexT] {
     given Conversion[Simplex, ChainElement[Simplex, CoefficientT]] =
       ChainElement.apply
->>>>>>> 38ae15d5743564e6a676dcf2456f6a7f88f6a460
   }
 }
